cc251bc9850ff94517966ec22e8ca32af8240f4e
Jakub <Wilk>
2022-12-08 09:49:44 -0600

    Initial Commit

---
+++ Pigapp/PigappApp.swift
@@ -0,0 +1,17 @@
+//
+//  PigappApp.swift
+//  Pigapp
+//
+//  Created by Jakub Wilk on 12/8/22.
+//
+
+import SwiftUI
+
+@main
+struct PigappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
